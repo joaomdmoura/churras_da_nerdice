e2b88178af36c8828fedf6231949accf02353a6a
João Moura <joaomdmoura@gmail.com>
2016-09-20 14:38:46 -0300

    lol

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'HTTParty'
+
+get '/' do
+  url = (params["url"]) ? params["url"] : 'http://maps.google.com/maps/api/geocode/json?address=Av.+Pref.+Hirant+Sanazar%2C+631'
+  response = HTTParty.get(url)
+  parsed = JSON.parse(response.body)["results"]
+  haml :index, format: :html5, locals: {result: parsed}
+end
