aed7576f14f06ad721645db6575627cd797d2584
João Moura <joaomdmoura@gmail.com>
2016-09-20 17:49:37 -0300

    adding bootstrap and interpolating string for api call

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
+# encoding: UTF-8
 require 'sinatra'
 require 'httparty'
 
 get '/' do
-  url = (params["url"]) ? params["url"] : 'http://maps.google.com/maps/api/geocode/json?address=Av.+Pref.+Hirant+Sanazar%2C+631'
+  addr     = URI.escape((params['url']) ? params['url'].gsub(' ', '+') : 'Av.+Pref.+Hirant+Sanazar%2C+631')
+  url      = "http://maps.google.com/maps/api/geocode/json?address=#{addr}"
   response = HTTParty.get(url)
-  parsed = JSON.parse(response.body)["results"]
+  parsed   = JSON.parse(response.body)['results']
   haml :index, format: :html5, locals: {result: parsed}
 end
